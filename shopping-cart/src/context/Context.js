a241a1bf62fc19c9eca21e41c39ec9ccdc3e1674
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-01-23 21:51:09 +0530

    faker api

--- shopping-cart/src/context/Context.js
+++ shopping-cart/src/context/Context.js
@@ -1,9 +1,35 @@
-const Context = () => {
-  return (
-    <div>
-      <h1>Context component</h1>
-    </div>
-  );
+import { faker } from "@faker-js/faker";
+import { createContext, useContext, useReducer } from "react";
+faker.seed();
+
+const Cart = createContext();
+
+const Context = ({ children }) => {
+  const products = [...Array(20)].map(() => ({
+    id: faker.string.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price().split(".")[0], // 15.00 => [15,00]
+    image: faker.image.url(),
+    inStock: faker.helpers.arrayElement([0, 2, 4, 6, 7, 9]),
+    quickDelivery: faker.datatype.boolean(),
+    ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
+  }));
+
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: products,
+    cart: [],
+  });
+
+  const [productState, productDispatch] = useReducer(productReducer, {
+    byStock: false,
+    quickDelivery: false,
+    byRating: 0,
+    searchQuery: "",
+  });
+  return <Cart.Provider value={{ state, dispatch, productState, productDispatch }}>{children}</Cart.Provider>;
 };
 
+export const CartState = () => {
+  return useContext(Cart);
+};
 export default Context;
